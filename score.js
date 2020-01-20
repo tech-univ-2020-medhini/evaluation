26cab103b5ebe1a26917e953dbad4860722021fa
medhinioak <medhinioak@gmail.com>
2020-01-20 11:47:51 +0530

    Refactored code and added comments

--- score.js
+++ score.js
@@ -1,36 +1,38 @@
 function score(throwsArray){
-	let calculatedScore = 0;
-	let turnNumber = 0;
-	if(throwsArray == undefined){
+	let calculatedScore = 0;       //Stores the score of the player
+	if(throwsArray == undefined){    //If no argument is passed, return the default(0)
 		return calculatedScore;
 	}
-	let firstThrow = true;
-	for(let i=0;i<throwsArray.length;i++){
-		if(turnNumber==10){
+	let firstThrow = true;         //Tracks whether the current throw is the first or second throw of the frame
+	let turnNumber = 0;             //Tracks the frame number
+	for(let i=0;i<throwsArray.length;i++){   //Traverse through the array and calculate the score
+		if(turnNumber==10){      //Stop counting score after 10 frames
 			break;
 		}
-		if(!firstThrow){
-			if(throwsArray[i]+throwsArray[i-1] < 10){
+		checkCase(i);
+	}
+	function checkCase(i){
+		if(!firstThrow){    //Check if it is the second throw of the frame
+			if(throwsArray[i]+throwsArray[i-1] < 10){   //Case when it is not a spare
 				calculatedScore = calculatedScore + throwsArray[i] + throwsArray[i-1];
 				turnNumber ++;
 			}
-			else{
+			else{         //Case when it is a spare
 				calculatedScore = calculatedScore + throwsArray[i+1] + throwsArray[i] + throwsArray[i-1];
 				turnNumber++;
 			}
-			firstThrow=!firstThrow;
+			firstThrow=!firstThrow;    //Change the tracker back to first throw 
 		}
-		else{
-			if(throwsArray[i] == 10){
+		else{           //Check if it is the first throw of the frame
+			if(throwsArray[i] == 10){   //Case when it is a strike
 				calculatedScore = calculatedScore + throwsArray[i] + throwsArray[i+1] + throwsArray[i+2];
 				turnNumber++;
                 
 			}
-			else{
+			else{           //If not a strike, change tracker to second throw
 				firstThrow=!firstThrow;
 			}
 		}
-		
 		
 	}
 	return calculatedScore;
