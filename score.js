7f072b8e4fd7d480eef0124f742818338f88f8ab
medhinioak <medhinioak@gmail.com>
2020-01-20 12:12:45 +0530

    Changed variable names to make code readable

--- score.js
+++ score.js
@@ -4,9 +4,9 @@ function score(throwsArray){
 		return calculatedScore;
 	}
 	let firstThrow = true;         //Tracks whether the current throw is the first or second throw of the frame
-	let turnNumber = 0;             //Tracks the frame number
+	let frameNumber = 0;             //Tracks the frame number
 	for(let i=0;i<throwsArray.length;i++){   //Traverse through the array and calculate the score
-		if(turnNumber==10){      //Stop counting score after 10 frames
+		if(frameNumber==10){      //Stop counting score after 10 frames
 			break;
 		}
 		checkCase(i);
@@ -15,18 +15,18 @@ function score(throwsArray){
 		if(!firstThrow){    //Check if it is the second throw of the frame
 			if(throwsArray[i]+throwsArray[i-1] < 10){   //Case when it is not a spare
 				calculatedScore = calculatedScore + throwsArray[i] + throwsArray[i-1];
-				turnNumber ++;
+				frameNumber ++;
 			}
 			else{         //Case when it is a spare
 				calculatedScore = calculatedScore + throwsArray[i+1] + throwsArray[i] + throwsArray[i-1];
-				turnNumber++;
+				frameNumber++;
 			}
 			firstThrow=!firstThrow;    //Change the tracker back to first throw 
 		}
 		else{           //Check if it is the first throw of the frame
 			if(throwsArray[i] == 10){   //Case when it is a strike
 				calculatedScore = calculatedScore + throwsArray[i] + throwsArray[i+1] + throwsArray[i+2];
-				turnNumber++;
+				frameNumber++;
                 
 			}
 			else{           //If not a strike, change tracker to second throw
