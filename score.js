41574fef53d3cb718644a50c42f1114db6187ed8
medhinioak <medhinioak@gmail.com>
2020-01-20 09:51:57 +0530

    Added second functionality: if there are no strikes or spares

--- score.js
+++ score.js
@@ -1,5 +1,17 @@
 function score(throwsArray){
 	let calculatedScore = 0;
+	if(throwsArray == undefined){
+		return calculatedScore;
+	}
+	let firstThrow = true;
+	for(let i=0;i<throwsArray.length;i++){
+		if(!firstThrow){
+			if(throwsArray[i]+throwsArray[i-1] < 10){
+				calculatedScore = calculatedScore + throwsArray[i] + throwsArray[i-1];
+			}
+		}
+		firstThrow=!firstThrow;
+	}
 	return calculatedScore;
 }
 
--- score.test.js
+++ score.test.js
@@ -5,4 +5,8 @@ describe('The scoring function', () => {
 		const result = score();
 		expect(typeof result).toBe('number');
 	});
+	it('Should return the sum of the array when there are no strikes or spares', () => {
+		const result = score([6, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+		expect(result).toBe(12);
+	});
 });
